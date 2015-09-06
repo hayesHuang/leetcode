4a41c2ca88ce3fe762b0dfed2e8d18a32f16d866
hyi <hyi1023@163.com>
2015-09-06 17:07:21 +0800

    测试

--- src/leetcode/ConvertSortedListtoBinarySearchTree.java
+++ src/leetcode/ConvertSortedListtoBinarySearchTree.java
@@ -2,6 +2,7 @@ package leetcode;
 
 public class ConvertSortedListtoBinarySearchTree {
     public TreeNode sortedListToBST(ListNode head) {
+		return null;
         
     }
 	public static void main(String[] args) {
--- src/leetcode/GasStation.java
+++ src/leetcode/GasStation.java
@@ -2,6 +2,7 @@ package leetcode;
 
 public class GasStation {
     public int canCompleteCircuit(int[] gas, int[] cost) {
+		return 0;
         
     }
 	public static void main(String[] args) {
